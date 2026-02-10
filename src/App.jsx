66c946008fb5c48e23a480789dd241c870b18cd2
Md. Piyash Mia <91769272+piyashltd@users.noreply.github.com>
2026-02-10 10:22:14 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+// File: src/App.jsx
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import ShowDetails from './pages/ShowDetails';
 import WatchPage from './pages/WatchPage';
+import ChannelDetails from './pages/ChannelDetails'; // নতুন ইম্পোর্ট
 
 function App() {
   return (
@@ -11,9 +13,9 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/channel/:id" element={<ChannelDetails />} /> {/* এই লাইনটি যোগ করুন */}
           <Route path="/show/:id" element={<ShowDetails />} />
           <Route path="/watch/:episodeId" element={<WatchPage />} />
-          {/* Add more routes as needed */}
         </Routes>
       </Layout>
     </BrowserRouter>
