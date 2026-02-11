24d6ac0b57127d1bdae54db61988a580ac1827cb
Md. Piyash Mia <91769272+piyashltd@users.noreply.github.com>
2026-02-11 16:33:15 +0600

    Added 10 new episodes via Bot

--- src/data/dummyData.js
+++ src/data/dummyData.js
@@ -86,7 +86,117 @@ export const shows = [
 
 // ------------------- EPISODES -------------------
 export const episodes = [
-  // --- Kurulus Osman Episodes ---
-  
+  // --- Kurulus Osman Episodes ---,
+
+  {
+    "id": "kurulus-osman-s01-e01",
+    "showId": "kurulus-osman",
+    "season": 1,
+    "episodeNumber": 1,
+    "title": "Kurulus Osman S01",
+    "thumbnail": "https://ik.imagekit.io/ottstream/tr:f-webp/KS%20S01E01.jpg",
+    "date": "Dec 1, 2020",
+    "duration": "40min",
+    "videoUrl": "https://stream.mux.com/ps02zMrbyQ702j4wM3DwvkJKkIb5F7w02dCC7zBjdjEUCs.m3u8"
+  },
+  {
+    "id": "kurulus-osman-s01-e02",
+    "showId": "kurulus-osman",
+    "season": 1,
+    "episodeNumber": 2,
+    "title": "Kurulus Osman S01",
+    "thumbnail": "https://ik.imagekit.io/ottstream/tr:f-webp/KS%20S01E02.jpg",
+    "date": "Dec 2, 2020",
+    "duration": "39min",
+    "videoUrl": "https://stream.mux.com/Kv02SRxY464dhFYN200qOfHwrrh9zwfTEDXpVnfhMjYUI.m3u8"
+  },
+  {
+    "id": "kurulus-osman-s01-e03",
+    "showId": "kurulus-osman",
+    "season": 1,
+    "episodeNumber": 3,
+    "title": "Kurulus Osman S01",
+    "thumbnail": "https://ik.imagekit.io/ottstream/tr:f-webp/KS%20S01E03.jpg",
+    "date": "Dec 3, 2020",
+    "duration": "41min",
+    "videoUrl": "https://stream.mux.com/PzertQau7QvY1hbCbU9XMK4ZZPjJAL7iHGmzF024ozEU.m3u8"
+  },
+  {
+    "id": "kurulus-osman-s01-e04",
+    "showId": "kurulus-osman",
+    "season": 1,
+    "episodeNumber": 4,
+    "title": "Kurulus Osman S01",
+    "thumbnail": "https://ik.imagekit.io/ottstream/tr:f-webp/KS%20S01E04.jpg",
+    "date": "Dec 4, 2020",
+    "duration": "40min",
+    "videoUrl": "https://stream.mux.com/Lrv3BM6G9Qx1pxRszW00VHihhLG00x6t5s01EG4YJ02Cvco.m3u8"
+  },
+  {
+    "id": "kurulus-osman-s01-e05",
+    "showId": "kurulus-osman",
+    "season": 1,
+    "episodeNumber": 5,
+    "title": "Kurulus Osman S01",
+    "thumbnail": "https://ik.imagekit.io/ottstream/tr:f-webp/KS%20S01E05.jpg",
+    "date": "Dec 5, 2020",
+    "duration": "39min",
+    "videoUrl": "https://stream.mux.com/3ov8WBdgZpjSfMM597buco00xJD9O009Z2HTcYf4q8LOo.m3u8"
+  },
+  {
+    "id": "kurulus-osman-s01-e06",
+    "showId": "kurulus-osman",
+    "season": 1,
+    "episodeNumber": 6,
+    "title": "Kurulus Osman S01",
+    "thumbnail": "https://ik.imagekit.io/ottstream/tr:f-webp/KS%20S01E06.jpg",
+    "date": "Dec 6, 2020",
+    "duration": "41min",
+    "videoUrl": "https://stream.mux.com/rD017zEQ91k8Mt828cD02j00d4BQ548bFlu9dtVdQhcSLc.m3u8"
+  },
+  {
+    "id": "kurulus-osman-s01-e07",
+    "showId": "kurulus-osman",
+    "season": 1,
+    "episodeNumber": 7,
+    "title": "Kurulus Osman S01",
+    "thumbnail": "https://ik.imagekit.io/ottstream/tr:f-webp/KS%20S01E07.jpg",
+    "date": "Dec 7, 2020",
+    "duration": "40min",
+    "videoUrl": "https://stream.mux.com/yEgQWNiA00eZlimSlilm3kTWOyfy401olPiTYyMVBaVoE.m3u8"
+  },
+  {
+    "id": "kurulus-osman-s01-e08",
+    "showId": "kurulus-osman",
+    "season": 1,
+    "episodeNumber": 8,
+    "title": "Kurulus Osman S01",
+    "thumbnail": "https://ik.imagekit.io/ottstream/tr:f-webp/KS%20S01E08.jpg",
+    "date": "Dec 8, 2020",
+    "duration": "39min",
+    "videoUrl": "https://stream.mux.com/jpAbpIQzGQxUoKEMbSp4FYayVY01NKjgSn5CrWJ89uU00.m3u8"
+  },
+  {
+    "id": "kurulus-osman-s01-e09",
+    "showId": "kurulus-osman",
+    "season": 1,
+    "episodeNumber": 9,
+    "title": "Kurulus Osman S01",
+    "thumbnail": "https://ik.imagekit.io/ottstream/tr:f-webp/KS%20S01E09.jpg",
+    "date": "Dec 9, 2020",
+    "duration": "41min",
+    "videoUrl": "https://stream.mux.com/RHLt8uT63GJkRAuX1cmas26ur3MTjw01jRSJLINV00Vk8.m3u8"
+  },
+  {
+    "id": "kurulus-osman-s01-e10",
+    "showId": "kurulus-osman",
+    "season": 1,
+    "episodeNumber": 10,
+    "title": "Kurulus Osman S01",
+    "thumbnail": "https://ik.imagekit.io/ottstream/tr:f-webp/KS%20S01E10.jpg",
+    "date": "Dec 10, 2020",
+    "duration": "40min",
+    "videoUrl": "https://stream.mux.com/0202O501BuwMS68eeaxtVToqXdfU00Q3Rtt1VutHkclEG6g.m3u8"
+  }
 
 ];
