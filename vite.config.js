35d1cff15a97c71813e268dd9f14bad288003451
Md. Piyash Mia <91769272+piyashltd@users.noreply.github.com>
2026-02-18 09:42:54 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      // অ্যান্ড্রয়েড টিভি এবং পুরোনো ভার্সনের মোবাইলের জন্য পলিফিল তৈরি করবে
+      targets: ['defaults', 'not IE 11', 'Android >= 5'],
+    }),
+  ],
 })
